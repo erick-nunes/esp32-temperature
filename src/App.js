05a02bc216935d57a01a55002b662147668ede9b
Erick Nunes <eu@ericknunes.dev>
2023-08-17 02:02:07 -0300

    Update readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({ temperatura: null, umidade: null });
+
+  useEffect(() => {
+    // Função para buscar os dados
+    const fetchData = () => {
+      fetch("http://192.168.15.111/data")
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(error => console.error("Erro ao buscar dados:", error));
+    };
+
+    // Buscar os dados imediatamente ao carregar o componente
+    fetchData();
+
+    // Configurar um intervalo para buscar os dados a cada 10 segundos
+    const intervalId = setInterval(fetchData, 1000);
+
+    // Limpar o intervalo quando o componente for desmontado
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Temperatura e Umidade</h1>
+      <p>Temperatura: <b> {data.temperatura}°C </b></p>
+      <p>Umidade: <b> {data.umidade}% </b></p>
     </div>
   );
 }
